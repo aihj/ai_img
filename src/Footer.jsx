a39fbd3c000dd4957c89c1a36e2254e28ac93ef4
Heejae Kim <heejae9102@gmail.com>
2024-08-18 16:26:50 +0900

    init

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+import React, { useState, useEffect } from "react";
+
+import './App.css';
+
+function Footer() {
+
+    return(
+        <div class ="info-page">
+            <Link to ="/info"> Classify </Link>
+
+
+        </div>
+    );
+}
+
+export default Footer;
